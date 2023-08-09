f87ce5c53585d2c6fe369b2d0aa09cc6b827b497
Svatosik <125908175+Svatosik@users.noreply.github.com>
2023-08-09 21:26:31 +0200

    v2

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,19 +1,25 @@
 import axios from 'axios'; // імпортуємо бібліотеку axios
 
-export function fetchBreeds() {
+axios.defaults.headers.common['x-api-key'] =
+  'live_lUgpZbgC3NGGA3aWJrXy0vUJgWER9dLhz7duFnCnXJso88wePXI8yHUKVnA2IYIB';
+const BASE_URL = ' https://api.thecatapi.com/v1';
+
+function fetchBreeds() {
   //декларація безагрументової функції fetchBreeds, яка буде експортуватись, данна функція повертає проміс зі всіма породами
-  return axios.get('https://api.thecatapi.com/v1/breeds').then(resp => {
-    return resp.data;
+  return axios.get(`${BASE_URL}/breeds`)
+        .then(resp => {
+            return resp.data;
   });
 }
 
-export function fetchCatByBreed(breedId) {
+function fetchCatByBreed(breedId) {
   //декларація безагрументової функції fetchBreeds, яка буде експортуватись, данна функція повертає проміс з інформацією про певну породу
-  return axios
-    .get(
-      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_v3AzDsPEsw05lGsv86KKH9kk3GHpRImfwD997cCTqYa0MG9BADoIYmgYTjuNQpvJ`
-    )
-    .then(resp => {
-      return resp.data;
-    });
+   return axios
+     .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+     .then(resp => {
+       return resp.data;
+     });
+
 }
+
+export { fetchCatByBreed, fetchBreeds };
